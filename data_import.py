b99ff791535408b2c1ccb495720b373dc663e54d
Annihilatrix <annihilatrix@katie.codes>
2016-05-08 00:32:33 -0700

    Set up virtualenv, created general TODO and data import to db document

---
+++ data_import.py
@@ -0,0 +1,10 @@
+# Populating a PostgreSQL database
+
+# Scrape one local theater's site for horror showtimes
+# Get movie information about that movie from IMDB using IMDbPY
+
+# Look into format of rss/atom feed for horror blog
+
+# Might be able to get location and theatre info from google maps API
+
+# See if python meetup api is still viable
