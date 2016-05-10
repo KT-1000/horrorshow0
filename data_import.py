41be315beb7cd8f18b3a4f8ac9f86811e588cab3
Annihilatrix <annihilatrix@katie.codes>
2016-05-09 17:59:01 -0700

    Couldn't get IMDbPY working, so starting BeautifulSoup parser to get movie info from IMDb

--- data_import.py
+++ data_import.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+from urllib import urlopen
 # Populating a PostgreSQL database
 
 # Scrape one local theater's site for horror showtimes
@@ -8,3 +10,13 @@
 # Might be able to get location and theatre info from google maps API
 
 # See if python meetup api is still viable
+# get html document
+html_doc = urlopen("http://www.imdb.com/search/title?genres=horror&sort=moviemeter,asc&title_type=feature")
+# search for horror movies by popularity in html
+# soup is one long string of html
+soup = BeautifulSoup(html_doc, 'html.parser')
+# get movie id and title from soup
+# form URL according to id in following format: http://www.imdb.com/title/<movie_id>/
+for result in soup.table:
+    # print each out
+    print result
